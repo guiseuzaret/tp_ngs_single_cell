197eaae7e9449e6a09841d486d325a06d9d46475
Guillaume Seuzaret <guillaume.seuzaret@ens-lyon.fr>
2020-11-18 13:41:31 +0000

    Fastq-dump script - commented

--- scripts/Data_download_script.sh
+++ scripts/Data_download_script.sh
@@ -1,15 +1,18 @@
 #! /bin/bash
 
+# Define a working directory
 data="/home/rstudio/data/mydatalocal/data"
 mkdir -p $data
 cd $data
 
+#define a directory where the data will be stored
 mkdir -p sra_data
 cd sra_data
 
-#head -10 /home/rstudio/data/mydatalocal/data/SRR_Acc_List.txt > SRR_partial.txt
+
 SRR=`cat /home/rstudio/data/mydatalocal/data/SRR_Acc_List.txt`
 
+#Extract data from the GO website
 for srr in $SRR
 do
 echo $srr
